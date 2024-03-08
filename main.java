5d400c8ee2e3e703fe6c13423674751225d5a545
Roberta Stira <151186486+robertastira@users.noreply.github.com>
2024-03-08 12:08:32 +0100

    test Abbassa e alza luminosità

--- main.java
+++ main.java
@@ -2,15 +2,18 @@ public class main {
 
     public static void main(String[] args){
 
-        video harryPotter = new video("Harry Potter e la Pietra Filosofale", 2,5,20);
+        video harryPotter = new video("Harry Potter e la Pietra Filosofale", 2,5,8);
         registrazioneaudio inTheShadows = new registrazioneaudio("In the shadows - The Rasmus", 5,5);
         immagine laGioconda = new immagine("La Gioconda di Da Vinci", 5);
         //harryPotter.play();
         //inTheShadows.play();
-        harryPotter.abbassaVolume();
-        harryPotter.play();
-        harryPotter.alzaVolume();
-        harryPotter.play();
+        //harryPotter.abbassaVolume();
+        //harryPotter.play();
+        //harryPotter.alzaVolume();
+        //harryPotter.play();
+        laGioconda.Show();
+        laGioconda.abbassaluminosita();
+        laGioconda.abbassaluminosita();
         laGioconda.Show();
 
 
